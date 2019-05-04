5b907f1a3323786bab2758796d63f8f8a6cb2675
Stuart Riffle <sriffle@gmail.com>
2019-05-04 09:44:52 -0700

    Removing eval engines

--- Testing/Engines/AdaChess/Makefile.bat
+++
@@ -1,6 +0,0 @@
-del *.ali *.o adachess.exe
-gcc src\signal.c -c
-gnatmake src\adachess -gnat2012 -gnatp -gnatn -O3 -f -c 
-gnatbind -x adachess
-gnatlink adachess signal.o
-@echo "Type 'adachess' to play!"
