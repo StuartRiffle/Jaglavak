6ebf80ee52c91b4cd60c41b8ee5cb2c92e9858b8
Stuart Riffle <sriffle@gmail.com>
2019-05-15 12:14:05 -0700

    Cleanup

---
+++ External/p3/include/pistache/version.h
@@ -0,0 +1,17 @@
+/* version.h
+   Kip Warner, 29 May 2018
+
+   Version constants
+*/
+
+#pragma once
+
+namespace Pistache {
+namespace Version {
+
+    static constexpr int Major = 0;
+    static constexpr int Minor = 0;
+    static constexpr int Patch = 001;
+} // namespace Version
+} // namespace Pistache
+
