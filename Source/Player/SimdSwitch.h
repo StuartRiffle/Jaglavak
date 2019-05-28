6a05275a2ff336c4137ed14ff67e0a0588961d93
Stuart Riffle <sriffle@gmail.com>
2019-05-28 10:29:48 -0700

    Allocator

---
+++ Source/Player/SimdSwitch.h
@@ -0,0 +1,28 @@
+// JAGLAVAK CHESS ENGINE (c) 2019 Stuart Riffle
+#pragma once
+
+static void PlayGamesSimd( const GlobalOptions* options, const PlayoutParams* params, const Position* pos, ScoreCard* dest, int count )
+{
+    int simdLevel = options->mDetectedSimdLevel;
+
+    if( !options->mEnableSimd )
+        simdLevel = 1;
+
+    if( options->mForceSimdLevel )
+        simdLevel = options->mForceSimdLevel;
+
+    int simdCount = (count + simdLevel - 1) / simdLevel;
+
+    extern void PlayGamesAVX512( const PlayoutParams* params, const Position* pos, ScoreCard* dest, int simdCount );
+    extern void PlayGamesAVX2(   const PlayoutParams* params, const Position* pos, ScoreCard* dest, int simdCount );
+    extern void PlayGamesSSE4(   const PlayoutParams* params, const Position* pos, ScoreCard* dest, int simdCount );
+    extern void PlayGamesX64(    const PlayoutParams* params, const Position* pos, ScoreCard* dest, int count );
+
+    switch( simdLevel )
+    {
+    case 8:   PlayGamesAVX512( params, pos, dest, simdCount ); break;
+    case 4:   PlayGamesAVX2(   params, pos, dest, simdCount ); break;
+    case 2:   PlayGamesSSE4(   params, pos, dest, simdCount ); break;
+    default:  PlayGamesX64(    params, pos, dest, count ); break;
+    }
+}
