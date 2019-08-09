4fc881675e22054242f27fb85948f3ddfecbec5c
Stuart Riffle <sriffle@gmail.com>
2019-08-09 11:35:11 -0700

    Tuning CNN

--- Testing/TrainPriorCalc.py
+++ Testing/TrainPriorCalc.py
@@ -43,15 +43,11 @@ def encode_position( board ):
     return encoded
 
 def encode_move( move ):
-    encoded = np.zeros( (64, 64), np.int8 )
-    encoded[move.from_square, move.to_square ] = 1
-    return encoded
-
-def encode_all_moves( board ):
-    encoded = np.zeros( (64, 64), np.int8 )
-    for move in board.legal_moves:
-        encoded[move.from_square, move.to_square] = 1
-    return encoded
+    src  = np.zeros( 64, np.int8 )
+    dest = np.zeros( 64, np.int8 )
+    src[move.from_square] = 1;
+    dest[move.to_square] = 1;
+    return src, dest
 
 def load_game_list(data_file_name):
     game_list = []
@@ -61,21 +57,20 @@ def load_game_list(data_file_name):
             game_list = json.loads( f.read().decode("utf-8") )
     return game_list
     
-
-
 def generate_test_data():
 
     ep_list = []
-    ebm_list = []
+    ebm_src_list = []
+    ebm_dest_list = []
 
     data_directory = "c:/dev/Jaglavak/Testing/Data/Games"
-    data_files_avail = glob.glob(data_directory + '/*.pgn.json.zip')
+    data_files_avail = glob.glob(data_directory + '/ccrl-4040-*.pgn.json.zip')
 
     for i in range( 1 ):
         data_file_name = data_files_avail[random.randrange( len( data_files_avail ) )]
         game_list = load_game_list( data_file_name )
 
-        print( "[" + str( i ) + "] Tap " + data_file_name )
+        print( "*** [" + str( i ) + "] Tapping " + data_file_name )
         for game in game_list:
                 moves  = game['moves']
                 result = game['result']
@@ -83,7 +78,8 @@ def generate_test_data():
                 move_list = moves.split( ' ' )
 
                 valid = len( move_list ) - 2
-                move_to_use = int( random.randrange( valid ) )
+                move_to_use = int( (random.randrange( valid ) + random.randrange( valid )) / 2 )
+                flip_board = False
 
                 if result == '1-0':
                     # make sure it's a white move
@@ -91,6 +87,7 @@ def generate_test_data():
                 elif result == '0-1':
                     # make sure it's a black move
                     move_to_use = move_to_use | 1
+                    flip_board = True
                 else:
                     continue
 
@@ -100,65 +97,100 @@ def generate_test_data():
 
                 target_move = board.parse_uci( move_list[move_to_use] )
 
+                if flip_board:
+                    # Everything is pres
+                    board = board.mirror()
+                    target_move = Move( 
+                        chess.square_mirror( target_move.from_square ), 
+                        chess.square_mirror( target_move.to_square ) )
+
+                assert( target_move in board.legal_moves() )
+
                 ep  = encode_position( board )
-                ebm = encode_move( target_move )
+                ebm_src, ebm_dest = encode_move( target_move )
 
                 ep_list.append( ep )
-                ebm_list.append( ebm )
+                ebm_src_list.append( ebm_src )
+                ebm_dest_list.append( ebm_dest )
 
     position_data = np.asarray( ep_list,  np.int8 )
-    bestmove_data = np.asarray( ebm_list, np.int8 )
+    bestmove_src_data = np.asarray( ebm_src_list, np.int8 )
+    bestmove_dest_data = np.asarray( ebm_dest_list, np.int8 )
 
     np.save( 'c:/temp/ep', position_data )
-    np.save( 'c:/temp/ebm', bestmove_data )
-    print( "*** TEST DATA GENERATED ***")
+    np.save( 'c:/temp/ebm_src', bestmove_src_data )
+    np.save( 'c:/temp/ebm_dest', bestmove_dest_data )
+    print( "*** NEXT DATASET GENERATED ***")
 
 
 # The position is represented by 6 planes of 8x8 (one plane for each piece type), plus
 # a silly /extra/ plane, representing the side to move, with all values 1 or -1.
-position_input = Input( shape = (7, 8, 8), name = 'position' )
 
-num_filters = 200
+
+def conv_layer( layer, kernel, filters ):
+    return Conv2D( filters, kernel_size = kernel, activation = 'relu', padding = 'same', data_format='channels_first' )( layer )
+
+def pool_layer( layer ):
+    return MaxPooling2D( data_format = 'channels_first' )( layer )
 
 def load_or_create_model():
     try:
         model = tensorflow.keras.models.load_model('c:/dev/Jaglavak/Testing/foo.h5' )
+        print( "*** MODEL LOADED ***" )
     except:
         global num_filters
 
-        # 8x8
-        conv1 = Conv2D( num_filters, kernel_size = 9, activation = 'relu', padding = 'same', data_format='channels_first' )( position_input )
-        conv1 = Conv2D( num_filters, kernel_size = 5, activation = 'relu', padding = 'same', data_format='channels_first' )( conv1 )
-        pool1 = MaxPooling2D( data_format='channels_first' )( conv1 )
-        
-        num_filters = num_filters * 2
+        position_input = Input( shape = (7, 8, 8), name = 'position' )
+        filters = 100
+        layer = position_input
 
-        # 4x4
-        conv2 = Conv2D( num_filters, kernel_size = 3, activation = 'relu', padding = 'same', data_format='channels_first' )( pool1 )
-        conv2 = Conv2D( num_filters, kernel_size = 3, activation = 'relu', padding = 'same', data_format='channels_first' )( conv2 )
-        pool2 = MaxPooling2D( data_format='channels_first' )( conv2 )
 
-        num_filters = num_filters * 2
+        layer = conv_layer( layer, 3, filters )
+        layer = conv_layer( layer, 3, filters )
+        layer = conv_layer( layer, 3, filters )
+        layer = conv_layer( layer, 3, filters )
+        layer = pool_layer( layer )
+        pool1 = layer
 
-        # 2x2
-        conv3 = Conv2D( num_filters, kernel_size = 3, activation = 'relu', padding = 'same', data_format='channels_first' )( pool2 )
-        conv3 = Conv2D( num_filters, kernel_size = 1, activation = 'relu', padding = 'same', data_format='channels_first' )( conv3 )
-        pool3 = MaxPooling2D( data_format='channels_first' )( conv3 )
+        filters = filters * 2
 
-        dense = Concatenate()( [Flatten()( pool3 ), Flatten()( position_input )] )
+        layer = conv_layer( layer, 3, filters )
+        layer = conv_layer( layer, 3, filters )
+        layer = conv_layer( layer, 3, filters )
+        layer = pool_layer( layer )
+        pool2 = layer
+
+        filters = filters * 2
+
+        layer = conv_layer( layer, 3, filters )
+        layer = conv_layer( layer, 3, filters )
+        layer = pool_layer( layer )
+        pool3 = layer
+
+        dense = Concatenate()( [
+            Flatten()( position_input ),
+            Flatten()( pool1 ), 
+            Flatten()( pool2 ), 
+            Flatten()( pool3 ), 
+            ] )
+        #dense = Flatten()( layer )
         dense = Dense( 2048, activation = 'relu' )( dense )
 
-        output_layer = Dense( 64 * 64, activation = 'softmax' )( dense )
-        output_array = Reshape( (64, 64) )( output_layer )
+        output_src  = Dense( 64, activation = 'softmax' )( dense )
+        output_dest = Dense( 64, activation = 'softmax' )( dense )
+#        outputs = Concatenate()( [output_src, output_dest], axis = 1 )
+
 
         adam = Adam()
 
-        model = Model( position_input, output_array ) 
+        model = Model( position_input, [output_src, output_dest] ) 
         model.compile( 
-            optimizer = adam,
-            loss = 'categorical_crossentropy', 
+            optimizer = 'sgd',
+            loss = 'categorical_crossentropy',
             metrics = ['categorical_accuracy'],
             )
+        model.save('c:/dev/Jaglavak/Testing/foo.h5' )
+        print( "*** MODEL GENERATED ***" )
 
     return model
 
@@ -178,9 +210,12 @@ if __name__ == '__main__':
     model = load_or_create_model()
     model.summary()
 
+    val_data = None
     try:
-        position_test_data = np.load( "c:/temp/test100_ep.npy" )
-        bestmove_test_data = np.load( "c:/temp/test100_ebm.npy" )
+        position_test_data = np.load( "c:/temp/test10_ep.npy" )
+        bestmove_test_src  = np.load( "c:/temp/test10_ebm_src.npy" )
+        bestmove_test_dest = np.load( "c:/temp/test10_ebm_dest.npy" )
+        val_data = (position_test_data, [bestmove_test_src, bestmove_test_dest])
     except:
         pass
 
@@ -188,19 +223,21 @@ if __name__ == '__main__':
         try:
             # FIXME for testing only
             position_data = np.load( "c:/temp/ep.npy" )
-            bestmove_data = np.load( "c:/temp/ebm.npy" )
+            bestmove_src  = np.load( "c:/temp/ebm_src.npy" )
+            bestmove_dest = np.load( "c:/temp/ebm_dest.npy" )
         except:
             generate_test_data()
 
         asyncgen = Process(target=generate_test_data)
         asyncgen.start()
 
-        model.fit( position_data, bestmove_data, 
-            validation_data = (position_test_data, bestmove_test_data),
-            epochs = 3,
-            batch_size = 100,
+        model.fit( position_data, [bestmove_src, bestmove_dest], 
+            #validation_data = (position_test_data, [bestmove_test_src, bestmove_test_dest]),
+            epochs = 2,
+            batch_size = 10,
             validation_split = 0.1,
             verbose = 2, 
+            shuffle = True,
             callbacks=[tensorboard] )
 
         asyncgen.join()
